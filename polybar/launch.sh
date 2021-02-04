728baa79d9308a4e7e4ddd00ef183bb223a9f852
José Eduardo <josee.edumont@gmail.com>
2021-02-03 22:13:23 -0300

    Initial commit

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload mybar -c ~/.config/polybar/config.ini &
+done
+
+
