d80d583fd153f18b977f91f0eb6c555900fdc240
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-01-18 21:55:38 -0300

    fix fish config

---
+++ fish/.config/fish/conf.d/fnm.fish
@@ -0,0 +1,3 @@
+# fnm
+set PATH "/home/josee/.local/share/fnm" $PATH
+fnm env | source
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,17 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if not status is-login
+	exec tmux
 end
+
+if status is-interactive
+end
+
+
+fish_add_path -a $HOME/.local/bin
+fish_add_path -a $HOME/.cargo/bin
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/josee/.ghcup/bin $PATH # ghcup-env
+
+# pnpm
+set -gx PNPM_HOME "/home/josee/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
