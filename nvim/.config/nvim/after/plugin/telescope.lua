c067cd2ed60147dd9b9c75e17cf14f751a51d5b5
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-02-15 18:03:46 -0300

    ignore large files on telescope preview

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -10,6 +10,13 @@ local silent_opts = {noremap = true, silent = true}
 telescope.setup{
 	defaults = {
 		file_ignore_patterns = {"node_modules", "esy.lock"},
+		preview = {
+			filesize_hook = function(filepath, bufnr, opts)
+				local max_bytes = 10000
+				local cmd = {"head", "-c", max_bytes, filepath}
+				require('telescope.previewers.utils').job_maker(cmd, bufnr, opts)
+			end
+		}
 	}
 
 }
